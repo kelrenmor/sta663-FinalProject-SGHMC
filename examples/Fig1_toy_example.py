de88cdc143673ccdb0d3e807145d1dc05bc7cb05
Kelly Moran <krmoran@g.clemson.edu>
2018-04-22 16:03:03 -0400

    adding HMC comparison for both examples, small amount of cleanup of both codes

--- examples/Fig1_toy_example.py
+++ examples/Fig1_toy_example.py
@@ -7,14 +7,17 @@ import seaborn as sns
 ### See pg. 6 of Chen et. al. for their results/comparison
 
 # Log likelihood function
-def U(theta, x):
+def U(theta):
     return(-2*theta**2 + theta**4)
 # True gradient
 gradU = jacobian(U, argnum=0)
-# Noisy gradient, based on what they do in the paper, with
-# \Delta \tilde{U}(\theta) = \Delta U(\theta) + N(0,4)
-def noisy_gradU(theta, x):
+# Noisy gradient, based on what they do in the paper for Fig 1
+def noisy_gradU(theta, x, n, batch_size):
+    '''Noisy gradient \Delta\tilde{U}(\theta)=\Delta U(\theta)+N(0,4)
+    Extra args (x, n, batch_size) for compatibility with sghmc()'''
     return -4*theta + 4*theta**3 + np.random.normal(0,2)
+# Set random seed
+np.random.seed(1234)
 # Don't actually need 'data' in this example, just use
 # it as a place-holder to fit into our function.
 n = 100
@@ -23,7 +26,7 @@ x = np.array([np.random.normal(0, 1, (n,1))]).reshape(-1,1)
 theta_0 = np.array([0.0]) # Initialize theta
 p = theta_0.shape[0]
 eta = 0.001 * np.eye(p) # make this small
-alpha = 0.05 * np.eye(p)
+alpha = 0.01 * np.eye(p)
 V = np.eye(p)
 batch_size = n # since we're not actually using the data, don't need to batch it
 niter = 500000 # Lots of iterations
@@ -32,6 +35,7 @@ samps_sghmc = sghmc(noisy_gradU, eta, niter, alpha, theta_0, V, x, batch_size)
 # plot the samples from the algorithm
 sns.kdeplot(samps_sghmc.reshape(-1))
 
+################################################################################
 
 # Use the hmc function from the pyhmc (at all default settings)
 # to sample from this distribution.
@@ -42,7 +46,16 @@ def logprob(theta):
     logp = -2*theta**2 + theta**4
     grad = -4*theta + 4*theta**3
     return logp, grad
-# run the HMC sampler
-samps_hmc = hmc(logprob, x0=theta_0, n_samples=niter) # use same niter as SGHMC
+# run the HMC sampler (use same theta_0 and niter as SGHMC)
+samps_hmc = hmc(logprob, x0=theta_0, n_samples=niter) 
+# plot the samples from the HMC algorithm
+sns.kdeplot(samps_hmc.reshape(-1))
+
+# The hmc() function with that many iterations goes kind of insane... 
+# How about less samples? Also redefine logprob(theta) to use funs from above
+def logprob(theta):
+    return U(theta, x, n, batch_size), gradU(theta, x, n, batch_size).reshape(1)
+# run the HMC sampler (use same theta_0 as above but fewer samples)
+samps_hmc = hmc(logprob, x0=theta_0, n_samples=500) # NOPE! Still looks bad.
 # plot the samples from the HMC algorithm
 sns.kdeplot(samps_hmc.reshape(-1))
--- examples/Mixture_of_Normals.py
+++ examples/Mixture_of_Normals.py
@@ -6,17 +6,10 @@ Created on Thu Apr 19 15:36:51 2018
 @author: isaaclavine
 """
 
-#import ad
-# import numpy as np
 import matplotlib.pyplot as plt
 import autograd.numpy as np
 from autograd import jacobian
 import seaborn as sns
-#from functools import partial
-
-
-#import sghmc_algorithm
-
 
 ## Example #1:
 ## Sampling from a mixture of normals in 1-D
@@ -25,40 +18,37 @@ import seaborn as sns
 
 def log_prior(theta):
     return(-(1/(2*10))*theta.T@theta)
-    
-    
+      
 def log_lik(theta, x):
     return(np.log(0.5 * np.exp(-0.5*(theta[0]-x)**2) + 0.5* np.exp(-0.5*(theta[1]-x)**2)))
-    
 
 def U(theta, x, n, batch_size):
     return(-log_prior(theta) - (n/batch_size)*sum(log_lik(theta, x)))
-    
-    
-# Setup the data
+       
+# Automatic differentiation to get the gradient
+gradU = jacobian(U, argnum=0)
+
+# Set random seed
+np.random.seed(1234)
+# Set up the data
 p = 2 #dimension of theta
 theta = np.array([-3.0, 3.0]).reshape(p,-1)
 n = 10000
 x = np.array([np.random.normal(theta[0], 1, (n,1)),
               np.random.normal(theta[1], 1, (n,1))]).reshape(-1,1)
 
-# Plot the density of x
+# Plot the empirical distribution of x (sampled from mixture of normals)
 sns.kdeplot(x.reshape(-1), bw=1)
-    
-# Automatic differentiation to get the jacobian
-gradU = jacobian(U, argnum=0)
 
-# Now for sampling
+## Initialize parameters and sample 
 
-# Initialize theta
+# Initialize mean parameters
 #theta_0 = np.random.normal(size=(p,1))
-theta_0 = theta
+theta_0 = theta # initialize at "true" value for testing
 
-# Initialize hyper parameters:
-
+# Initialize tuning parameters:
 # learning rate
 eta = 0.01/n * np.eye(p)
-
 # Friction rate
 alpha = 0.1 * np.eye(p)
 
@@ -67,34 +57,38 @@ V = np.eye(p)*1
 niter = 100
 batch_size=1000
 
-
 # Run sampling algorithm
 samps = sghmc(gradU, eta, niter, alpha, theta_0, V, x, batch_size)
 
-# Plot the density of each theta
-sns.kdeplot(samps[0,:])
+# See below that the mean estimates are VERY precice
+# using this sampler, e.g. for \mu_0 we have samples in the range of
+# roughly 2.96 to 3.04, and for \mu_1 from -3.05 to -2.98 
+
+# Plot the density of each mean in mixture
+sns.kdeplot(samps[0,:]) # should be -3
+sns.kdeplot(samps[1,:]) # should be 3
 
 # Plot the joint density
 sns.kdeplot(samps[0,:], samps[1,:])
 
 
+################################################################################
 
-### Easy test example:
-def U(theta, x, n=1, batch_size=1):
-    return(-2*theta**2 + theta**4)    
-n = 10000
-x = np.array([np.random.normal(0, 1, (n,1))]).reshape(-1,1)
-gradU = jacobian(U, argnum=0)
-theta_0 = np.array([0.0])
-niter = 200
-p=1
-eta = 0.01 * np.eye(p)
-alpha = 0.05 * np.eye(p)
-V = np.eye(p)*4
-batch_size = 100
-samps = sghmc(gradU, eta, niter, alpha, theta_0, V, x, batch_size)
+# Use the hmc function from the pyhmc (at all default settings)
+# to sample from this distribution.
+# See https://pythonhosted.org/pyhmc/ for function details.
+from pyhmc import hmc
 
-sns.kdeplot(samps.reshape(-1))
+# define your probability distribution
+# note some fiddling to get dimensions to be compatible
+def logprob(theta):
+    logp = np.sum(U(theta, x=x, n=n, batch_size=n))
+    gradu = gradU(theta, x=x, n=n, batch_size=n).reshape((-1,))
+    return logp, gradu
 
-
-
+# run the HMC sampler 
+# ideally would use same theta_0 and niter as SGHMC, 
+# but computing the full gradient is prohibtively slow!!
+samps_hmc = hmc(logprob, x0=theta_0.reshape((-1)), n_samples=100) 
+# plot the samples from the HMC algorithm (jointly)
+sns.kdeplot(samps[0,:], samps[1,:])
